a66067e679136d910320774b3232540a800c7f22
Juliagit config --global user.email your_email@example.comjulia.byrne209@gmail.com <julia.byrne209@gmail.com>
2019-09-17 14:23:35 -0500

    String changed from all uppercase to all lowercase

--- src/DemoCase.java
+++ src/DemoCase.java
@@ -5,6 +5,10 @@ public class DemoCase {
         String str="HELLO";
         int len=str.length();
         System.out.println("The length of the string is " + len);
+        for(int i=0; i<len; i++){
+            char c=Character.toLowerCase(str.charAt(i));
+            System.out.print(c);
+        }
 
     }
 }
