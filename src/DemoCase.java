4a4a8a973ead31113dba24009bc396177777301f
Juliagit config --global user.email your_email@example.comjulia.byrne209@gmail.com <julia.byrne209@gmail.com>
2019-09-17 14:14:53 -0500

    first commit

---
+++ src/DemoCase.java
@@ -0,0 +1,10 @@
+public class DemoCase {
+    public static void main(String[] args){
+        System.out.println("Text Processing Demo");
+
+        String str="HELLO";
+        int len=str.length();
+        System.out.println("The length of the string is " + len);
+
+    }
+}
